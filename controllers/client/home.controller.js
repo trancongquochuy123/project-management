c3985ce38f647fa255659b199ddbae211b1a1a8c
Quoc Huy <trancongquochuy123@gmail.com>
2025-04-03 20:29:26 +0700

    Cấu trúc lại thư mục View

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,6 @@
 module.exports.index = (req, res) => {
-    res.render("client/pages/home/index.pug");
+    res.render("client/pages/home/index.pug", {
+        pageTitle: "Home",
+        description: "Welcome to our home page!"
+    });
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,6 @@
 module.exports.index = (req, res) => {
-    res.render("client/pages/products/index.pug");
+    res.render("client/pages/products/index.pug", {
+        pageTitle: "Products",
+        description: "Welcome to our products page!"
+    });
 }
