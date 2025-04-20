c3d9381772d7b022ab3893f924cbe910aff4624f
Quoc Huy <trancongquochuy123@gmail.com>
2025-04-20 22:25:50 +0700

    Cài đặt Mongonoose

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
 require('dotenv').config();
 
+const database = require('./config/database.js');
 const route = require('./routes/client/index.route.js');
+
+database.connect();
+
 const app = express();
 const port = process.env.PORT ;
 
---
+++ models/product.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    rating: { type: Number, required: true },
+    comment: { type: String },
+    date: { type: Date, required: true },
+    reviewerName: { type: String, required: true },
+    reviewerEmail: { type: String, required: true }
+});
+
+const productSchema = new mongoose.Schema(
+    {
+        id: { type: Number, unique: true, required: true },
+        title: { type: String, required: true },
+        description: { type: String },
+        category: { type: String },
+        price: { type: Number, required: true },
+        discountPercentage: { type: Number },
+        rating: { type: Number },
+        stock: { type: Number },
+        tags: [String],
+        brand: { type: String },
+        sku: { type: String },
+        weight: { type: Number },
+        dimensions: {
+            width: { type: Number },
+            height: { type: Number },
+            depth: { type: Number }
+        },
+        warrantyInformation: { type: String },
+        shippingInformation: { type: String },
+        availabilityStatus: { type: String },
+        reviews: [reviewSchema],
+        returnPolicy: { type: String },
+        minimumOrderQuantity: { type: Number },
+        meta: {
+            createdAt: { type: Date },
+            updatedAt: { type: Date },
+            barcode: { type: String },
+            qrCode: { type: String }
+        },
+        images: [String],
+        thumbnail: { type: String },
+        deleted: { type: Boolean, default: false }
+    }
+);
+
+const Product = mongoose.model('Product', productSchema, 'products');
+
+module.exports = Product;
