04ca48c30e25d160728611a0da7c1afa1feef417
Quoc Huy <trancongquochuy123@gmail.com>
2025-04-28 20:35:51 +0700

    1.2 Xây dựng layout chung cho dashboard

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 require('dotenv').config();
 
 const database = require('./config/database.js');
+
+const systemConfig = require('./config/system.js');
+
 const route = require('./routes/client/index.route.js');
 const routeAdmin = require('./routes/admin/index.route.js');
 
@@ -13,6 +16,9 @@ const port = process.env.PORT ;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+// App Locals variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin; 
 
 app.use(express.static('public'));
 
