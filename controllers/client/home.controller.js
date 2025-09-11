40ad43e4527b24c9a7b03fc372c55b749df66d95
Quoc Huy <trancongquochuy123@gmail.com>
2025-09-11 18:39:29 +0700

    feat: Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -4,25 +4,35 @@ const { priceNewProduct } = require('../../helper/products.js');
 // [GET] /
 module.exports.index = async (req, res) => {
     try {
+        // Featured Products
         const featuredProducts = await Product.find({ feature: '1', deleted: false, status: 'active' })
             .populate('product_category_id', 'title')
             .limit(8)
             .exec();
 
         const newProducts = priceNewProduct(featuredProducts);
+        // End Featured Products
+
+        // Sort by createdAt desc
+        const products = await Product.find({ deleted: false, status: 'active' })
+            .populate('product_category_id', 'title')
+            .sort({ createdAt: 'desc' })
+            .limit(8)
+            .exec();
+
+        const newProductsSorted = priceNewProduct(products);
 
         res.render("client/pages/home/index.pug", {
             pageTitle: "Home",
             description: "Welcome to our home page!",
             featuredProducts: featuredProducts,
             products: newProducts,
+            productsSorted: newProductsSorted,
         });
 
     } catch (err) {
         console.error("Error fetching products:", err);
         res.status(500).send("Internal Server Error");
     }
-
-
 }
 
